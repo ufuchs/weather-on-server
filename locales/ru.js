2757ce9837c7645fd9f3bdc9c9958bae28e10a9a
Uli Fuchs <ufuchs@gmx.com>
2013-05-31 15:04:14 +0200

    added: ru, tr

---
+++ locales/ru.js
@@ -0,0 +1,11 @@
+{
+    "dayOfYear": "{{doy}}-й день года",
+    "today": "cегодня",
+    "tomorrow": "завтра",
+    "sunrise": "вс",
+    "sunset": "зт",
+    "minimal": "мин",
+    "maximal": "макс",
+    "hour": "ч",
+    "min": "мин"
+}
---
+++ locales/tr.js
@@ -0,0 +1,11 @@
+{
+    "dayOfYear": "{{doy}}.Senenin Günü",
+    "today": "Bügün",
+    "tomorrow": "Yarin",
+    "sunrise": "GD",
+    "sunset": "GB",
+    "minimal": "Min",
+    "maximal": "Max",
+    "hour": "saat",
+    "min": "dakika"
+}
