07d4ae205eae922c2178b6dacd2b8ac5999ced58
Uli Fuchs <ufuchs@gmx.com>
2013-05-31 16:12:03 +0200

    added: cs.js

---
+++ locales/cs.js
@@ -0,0 +1,11 @@
+{
+    "dayOfYear": "{{doy}}.den v roce",
+    "today": "dnes",
+    "tomorrow": "zítra",
+    "sunrise": "vs",
+    "sunset": "zs",
+    "minimal": "min",
+    "maximal": "max",
+    "hour": "hod",
+    "min": "min"
+}
