efeef11ca3614a2ceed6260ceac68856e8d8d6dc
ufuchs <ufuchs@gmx.com>
2013-06-15 19:42:03 +0200

    introduced: locale specifics

---
+++ locales/locales.js
@@ -0,0 +1,14 @@
+/*jslint node:true*/
+
+'use strict';
+
+module.exports = {
+
+    locales: {
+        locales: ['cs', 'de', 'en', 'pl', 'ru', 'tr']
+    },
+
+    iso3166ToLocale: {
+        "cz" : "cs"
+    }
+};
