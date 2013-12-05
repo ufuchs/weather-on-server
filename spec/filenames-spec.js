ae8126bb05424733dd646a842d1930eaab6fb7fa
ufuchs <ufuchs@gmx.com>
2013-12-05 22:47:24 +0100

    '/spec' added

---
+++ spec/filenames-spec.js
@@ -0,0 +1,46 @@
+/*jslint node: true */
+/*jslint todo: true */
+
+'use strict';
+
+var
+    filenames = require('../lib/filenames.js'),
+    cfg = require('../weather-config.js'),
+    location = {
+        id: 1,
+        lon: ["13.4542", "13° 27"],
+        lat: ["52.5158", "52° 31'"],
+        name: "Germany/Berlin",
+        lang: "ru",
+        device: "kindle4nt",
+        period: 0
+
+    };
+
+
+filenames(cfg);
+
+//
+//
+//
+describe("locations", function () {
+
+    it("should work", function () {
+        expect(filenames).not.toBe(null);
+    });
+
+
+    it("should work", function () {
+
+
+        filenames.get(location, function (err, data) {
+            console.log(data);
+            expect(data).not.toBe(null);
+        });
+
+
+
+    });
+
+
+});
---
+++ spec/wunderground-spec.js
@@ -0,0 +1,80 @@
+/*jslint node: true */
+/*jslint todo: true */
+
+'use strict';
+
+var cfg = require('../weather-config.js'),
+    wunderground = require('../lib/provider/wunderground.js'),
+    proxy = process.env.HTTP_PROXY || process.env.http_proxy,
+    apikey = process.env.WUNDERGROUND_KEY,
+
+    location = {
+        id: 1,
+        lon: ["13.4542", "13° 27"],
+        lat: ["52.5158", "52° 31'"],
+        name: "Germany/Berlin",
+        lang: "ru",
+        device: "kindle4nt",
+        period: 0
+
+    },
+
+    params = {
+        location : {
+            id: 1,
+            lon: ["13.4542", "13° 27"],
+            lat: ["52.5158", "52° 31'"],
+            name: "Germany/Berlin",
+            lang: "ru",
+            device: "kindle4nt",
+            period: 0
+
+        },
+        weather : {},
+        json : null
+    };
+
+
+wunderground(proxy, apikey, 3600);
+
+describe("wunderground", function () {
+
+    var flag;
+
+    it("valid reference of 'params'", function() {
+        expect(params).not.toBe(null);
+    });
+
+    it("valid reference of 'wunderground'", function() {
+        expect(wunderground).not.toBe(null);
+    });
+
+    it("valid reference of 'wunderground.weather'", function() {
+        expect(wunderground.weather).not.toBe(null);
+    });
+
+
+    it("getWeather", function () {
+
+        runs(function () {
+            flag = false;
+            wunderground.useTestData(false);
+            wunderground.getWeather(params, function (err, p) {
+                flag = true;
+//                console.log(p.weather);
+            });
+        });
+
+        waitsFor(function() {
+            return flag;
+        }, "The Value should be incremented", 10000);
+
+        runs(function() {
+            expect(flag).toEqual(true);
+        });
+
+
+    });
+
+
+});
