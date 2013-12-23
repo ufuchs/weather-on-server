c8cc59a36b81df005325e9d16f59c6ad6e8fb138
Uli Fuchs <ufuchs@gmx.com>
2013-12-23 17:19:51 +0100

    'lib/configurator' optimized

--- weather-config.js
+++ weather-config.js
@@ -2,42 +2,87 @@
 
 'use strict';
 
-module.exports = {
+var path = require('path'),
 
-    provider : {
-        downloader : require('./lib/downloader.js'),
-        query : require('./lib/provider/wunderground/query.js'),
-        extractor : require('./lib/provider/wunderground/extractor.js')
-    },
+    appCfg = {
 
-    production : {
+        provider : {
+            downloader : require('./lib/downloader.js'),
+            query : require('./lib/provider/wunderground/query.js'),
+            extractor : require('./lib/provider/wunderground/extractor.js')
+        },
 
-        dir : './public/weather',
+        production : {
 
-        files : {
+            dir : './public/weather',
 
-            names : {
-                weatherPng : 'weather.png'
+            files : {
+
+                names : {
+                    weatherPng : 'weather.png'
+                },
+
+                quantity : {
+                    kindle4nt : 1,
+                    df3120 : 4
+                }
+
             },
 
-            quantity : {
-                kindle4nt : 1,
-                df3120 : 4
-            }
+            /* expire time in seconds of the output files*/
+            expires : 3600
 
         },
 
-        /* expire time in seconds of the output files*/
-        expires : 3600
-
-    },
-
-    templatesPool : {
-        dir : './templates',
-        devices : {
-            kindle4nt : '600x800.svg',
-            df3120 : '320x240.svg'
+        templatesPool : {
+            dir : './templates',
+            devices : {
+                kindle4nt : '600x800.svg',
+                df3120 : '320x240.svg'
+            }
         }
-    }
 
+    };
+
+module.exports = appCfg;
+
+//
+//
+//
+appCfg.getFilesQuantity = function (device) {
+    return appCfg.production.files.quantity[device];
 };
+
+//
+//
+//
+appCfg.getProductionDir = function () {
+    return path.resolve(appCfg.production.dir);
+};
+
+//
+//
+//
+appCfg.getSvgTemplate = function (device) {
+    return path.join(path.resolve(appCfg.templatesPool.dir),
+        appCfg.templatesPool.devices[device]);
+};
+
+appCfg.getDeviceDir = function (device) {
+    return path.join(path.resolve(appCfg.production.dir), device);
+};
+
+//
+//
+//
+appCfg.getTargetDir = function (device, location) {
+    return path.join(appCfg.getDeviceDir(device), location);
+};
+
+//
+//
+//
+appCfg.getPngFilename = function () {
+    return appCfg.production.files.names.weatherPng;
+};
+
