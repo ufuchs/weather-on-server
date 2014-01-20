daebf6f5bc58745aa02d9a568524dd671dd99403
ufuchs <ufuchs@gmx.com>
2014-01-20 20:33:39 +0100

    'createWfo' removed

--- spec/spec-config.js
+++ spec/spec-config.js
@@ -3,66 +3,11 @@
 
 'use strict';
 
-module.exports.location = {
+module.exports.request = {
     id: 1,
-    lon: ["13.4542", "13° 27"],
-    lat: ["52.5158", "52° 31'"],
     name: "Germany/Berlin",
     lang: "ru",
     device: "kindle4nt",
     period: 0
-
-};
-
-module.exports.wfo = {
-    location : {
-        id: 1,
-        lon: ["13.4542", "13° 27"],
-        lat: ["52.5158", "52° 31'"],
-        name: "Germany/Berlin",
-        lang: "ru",
-        device: "kindle4nt",
-        period: 0
-
-    },
-    weather : {},
-    json : null,
-    localized : {
-        common : {},
-        header : {
-            date : null,
-            doy : null,
-            sun : {},
-            forecast : {}
-        },
-        forecastday : []
-    }
-};
-
-module.exports.createWfo = function (location) {
-
-    return {
-        cfg : {
-            svgTemplate : null,
-            location : location,
-            filenames : {
-                svg : {},
-                png : {}
-            }
-
-        },
-        json : null,
-        weather : {},
-        localized : {
-            common : {},
-            header : {
-                date : null,
-                doy : null,
-                sun : {},
-                forecast : {}
-            },
-            forecastday : []
-        }
-    };
 
 };
