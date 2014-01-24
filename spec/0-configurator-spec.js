6369feaed655e9297c55f40a8cc061d61c8ab8ec
ufuchs <ufuchs@gmx.com>
2014-01-24 18:02:38 +0100

    re-animated

--- spec/0-configurator-spec.js
+++ spec/0-configurator-spec.js
@@ -6,23 +6,16 @@
 var configurator = require('../lib/configurator.js'),
     path = require('path'),
     appcfg = require('../weather-config.js'),
-    fn = require("when/function");
-//    location = require('./spec-config.js').location;
+    request = require('./spec-config.js').request,
+    fn = require("when/function"),
+    utils = require('../lib/utils.js');
 
 //
 //
 //
 describe("configurator", function () {
 
-    var wfo,
-        location = {
-            id: 1,
-            name: "Germany/Berlin",
-            lang: "ru",
-            device: "kindle4nt",
-            period: 0
-
-        };
+    var wfo = utils.createWfo(request);
 
     configurator(appcfg);
 
@@ -39,7 +32,7 @@ describe("configurator", function () {
 
             ready = false;
 
-            configurator.configure(location)
+            configurator.configure(wfo)
                 .then(function (result) {
                     ready = true;
                     wfo = result;
@@ -55,20 +48,20 @@ describe("configurator", function () {
 
         runs(function() {
             expect(ready).toEqual(true);
-            console.log('wfo.cfg : ', wfo.cfg);
+//            console.log('wfo.cfg : ', wfo.cfg);
         });
 
     });
 
     it("should has a SVG template", function () {
 
-        var expected = appcfg.templatesPool.devices[location.device],
+        var expected = appcfg.templatesPool.devices[request.device],
             actual = path.basename(wfo.cfg.filenames.svg.template);
 
         expect(expected).toEqual(actual);
     });
 
-    it("should has a CCS file", function () {
+    it("should has a CSS file", function () {
         var expected = 'ru-kindle4nt.css',
             actual = path.basename(wfo.cfg.filenames.svg.cssFile);
         expect(expected).toEqual(actual);
