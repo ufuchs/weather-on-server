160d1aa84a7d902aa9bb28cb70c119ea45224c15
ufuchs <ufuchs@gmx.com>
2014-07-17 23:10:57 +0200

    wqorking on tests

--- spec/9-timebase-spec.js
+++ spec/9-timebase-spec.js
@@ -10,14 +10,11 @@ var MathU = require('../lib/posas/util/MathU.js'),
     x,
     y;
 
-
-
-
 up = tb.update(new Date());
 
-x = up.gmst0 * 240;
+x = (360 * 0.81) % 360;
 
-console.log(af.formatTime(MathU.deg2hhmmss(up.gmst0)  , 1));
+console.log(af.formatTime(MathU.deg2hhmmss(x)  , 1));
 
 describe("timebase", function () {
 
@@ -36,7 +33,8 @@ describe("timebase", function () {
             [1999, 1, 1.0, 2451179.5],
             [1987, 1, 27.0, 2446822.5],
             [1900, 1, 1.0, 2415020.5],
-            [1600, 1, 1.0, 2305447.5]
+            [1600, 1, 1.0, 2305447.5],
+            [1957, 10, 4.81, 2436116.31],  // launch time of Sputnik One
         ];
 
         dates.forEach(function (item) {
@@ -46,27 +44,54 @@ describe("timebase", function () {
 
     });
 
-    it("should calculate a JD by a Gregorian Calender date", function () {
+    it("should calculate the hour, min, sec of a day into a range between 0 =< x < 1 ", function () {
+
+        var actual,
+            hours = [
+            [0, 0, 0, 0],    // midnight
+            [12, 0, 0, 0.5]  // noon
+        ];
+
+        hours.forEach(function (item) {
+            actual = (MathU.hhmmss2dec.apply(this, item.slice(0, 3)));
+            expect(item[3]).toBe(actual);
+        });
+
+    });
+
+
+    it("should calculate a JD by a Gregorian Calender date and with the day time", function () {
 
         var actual,
             dates = [
-            // @see: MEEUS, Astronomical Algorithms (Second Edition), p62
-            [2014, 7, 17.0, 2456855.5],
-            [2000, 1, 1.5, 2451545.0],
-            [1999, 1, 1.0, 2451179.5],
-            [1987, 1, 27.0, 2446822.5],
-            [1900, 1, 1.0, 2415020.5],
-            [1600, 1, 1.0, 2305447.5]
-        ];
+                // @see: MEEUS, Astronomical Algorithms (Second Edition), p62
+                [2014, 7, 17, 0, 0, 0, 2456855.5],     // midnight
+                [2000, 1, 1, 12, 0, 0,  2451545.0],    // noon
+                [1957, 10, 4, 19, 26, 24, 2436116.31], // launch time of Sputnik One
+            ];
 
         dates.forEach(function (item) {
-            actual = (MathU.julianDay.apply(this, item.slice(0, 3)));
-            expect(item[3]).toBe(actual);
+            actual = (MathU.julianDayA.apply(this, item.slice(0, 6)));
+            expect(item[6]).toBe(actual);
         });
 
     });
 
 
+    it("should calculate ", function () {
+
+        var actual,
+            deg = [
+                [0, { sign : 1, hh : 0, mm : 0, ss : 0 }],    // midnight
+                [180, { sign : 1, hh : 12, mm : 0, ss : 0 }]  // noon
+            ];
+
+        deg.forEach(function (item) {
+            actual = (MathU.deg2hhmmss.apply(this, item.slice(0)));
+            expect(item[1]).toBe(actual);
+        });
+
+    });
 
 
 
