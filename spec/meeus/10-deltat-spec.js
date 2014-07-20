7fdeda90206b5d5cf906f958614f02d32b9fc2b6
ufuchs <ufuchs@gmx.com>
2014-07-20 22:54:27 +0200

    added: NASA ΔT calculation

---
+++ spec/meeus/10-deltat-spec.js
@@ -0,0 +1,21 @@
+/*jslint node: true */
+/*jslint todo: true */
+/*jslint nomen: true */
+
+var deltaT = require('./../../lib/posas/meeus/deltat/deltat.js');
+
+describe("Calcultes ΔT", function () {
+
+    it("for years after 2000", function () {
+
+        console.log('Nasa ', 2005, deltaT.poly2005to2050Nasa(2005));
+        console.log('MEEUS', 2005, deltaT.polyAfter2000Meeus(2005));
+
+        console.log('Nasa ', 2010, deltaT.poly2005to2050Nasa(2010));
+        console.log('MEEUS', 2010, deltaT.polyAfter2000Meeus(2010));
+
+
+    });
+
+});
+
