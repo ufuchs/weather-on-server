251af8baf016499cc2e054fa21f2170a8b70a61b
ufuchs <ufuchs@gmx.com>
2014-07-22 22:59:03 +0200

    adjust new 'julian.js'

--- spec/meeus/12-sidereal-spec.js
+++ spec/meeus/12-sidereal-spec.js
@@ -10,56 +10,22 @@ var sidereal = require('./../../lib/posas/meeus/sidereal/sidereal.js'),
 //
 //
 //
-describe("JD to century and fraction of the day", function () {
-
-    var jd = jd_of_1987_Apr_10;
-
-    it("returns the same day with f = 0", function () {
-
-        var actual = sidereal.jdToCFrac(jd),
-            expected_cen = -0.12729637234770705,
-            expected_f = 0;
-
-        // { jd0: 2446895.5, f: 0 }
-        expect(actual.cen).toBe(expected_cen);
-        expect(actual.dayFrac).toBe(expected_f);
-
-    });
-
-    it("returns the same day with f = 0.8", function () {
-
-        var actual = sidereal.jdToCFrac(jd + 0.8),
-            expected_cen = -0.12729637234770705,
-            expected_f =  0.7999999998137355;
-
-        // { jd0: 2446895.5, f: 0 }
-        expect(actual.cen).toBe(expected_cen);
-        expect(actual.dayFrac).toBe(expected_f);
-
-    });
-
-
-});
-
-//
-//
-//
 describe("Sidereal Time at Greenwich", function () {
 
     it("in pure seconds at 00:00", function () {
 
+        // (m)ean0UT
         var actual = sidereal.mean0UT(jd_of_1987_Apr_10),
-            expected_gmst0 = -1075753.63317289,
-            expected_dayFrac = 0;
+            expected_gmst0 = -1075753.63317289;
 
         expect(actual.gmst0).toBe(expected_gmst0);
-        expect(actual.dayFrac).toBe(expected_dayFrac);
 
     });
 
     // Meeus, p. 88, part of example 12.a
     it("normalized like the seconds of a day at 00:00", function () {
 
+        // (M)ean0UT
         var actual = sidereal.Mean0UT(jd_of_1987_Apr_10),
             expected = 47446.36682711006;
 
