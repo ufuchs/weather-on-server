fb96ecc2d6be82f472246f8cbe0cb10908517fbf
ufuchs <ufuchs@gmx.com>
2014-07-24 23:26:54 +0200

    apply more tests

--- spec/meeus/25-solar-spec.js
+++ spec/meeus/25-solar-spec.js
@@ -15,15 +15,19 @@ var base = require('./../../lib/posas/meeus/base/base.js'),
 describe("Solar Coordinates", function () {
 
     var jd = jd_of_1992_Oct_13,
+
+        // To make the calculation resistend against race condition, we hold all
+        // results into an object.
+        // This object will be passed through all calculations.
         s = {};
 
     s.T = julian.j2000Century(jd);
 
-    it("needs the param 'T'", function () {
+    it("needs the param 'T' at first. It's the base of all further calculations", function () {
 
         // Comments refer to the values of:
-        // MEEUS, Astronomical Algorithms (Second Edition), p. 165
-        // example 25.a
+        //   MEEUS, Astronomical Algorithms (Second Edition), p. 165
+        //   example 25.a
         var T = -0.07218343600273786;
         //  T = -0.072183436;
 
@@ -31,11 +35,11 @@ describe("Solar Coordinates", function () {
 
     });
 
-    it("uses param 'T'", function () {
+    it("calculates finally the values of Sun's true longitude and true anomaly", function () {
 
         // Comments refer to the values of:
-        // MEEUS, Astronomical Algorithms (Second Edition), p. 165
-        // example 25.a
+        //   MEEUS, Astronomical Algorithms (Second Edition), p. 165
+        //   example 25.a
         var actual = solar.calcTrueLonAndTrueAnomaly(s),
             L0   = 201.80719650670744,
         //  L0   = 201.80720
@@ -60,25 +64,47 @@ describe("Solar Coordinates", function () {
 
     });
 
-    // 1992 Oct 13 0h TD
+    // > check
+    it("calculates the Sun's radius vector", function () {
+
+        // Comments refer to the values of:
+        //   MEEUS, Astronomical Algorithms (Second Edition), p. 165
+        //   example 25.a
+        var actual = solar.calcRadiusVector(s),
+            R  = 0.9976628986428048,
+        //  R  = 0.99766
+            e = 0.01670560028506457;
+        //  e = 0.016711668
+
+        expect(s.e).toBe(e);            // eccentricity of the Earth
+        expect(s.R).toBe(R);            // Sun's radius vector
+
+    });
+
+    // > check
+    it("calculates the Sun's apparent longitude", function () {
+
+        // Comments refer to the values of:
+        //   MEEUS, Astronomical Algorithms (Second Edition), p. 165
+        //   example 25.a
+        var actual = solar.calcAppLon(s),
+            Ω  = 264.65,     // !mismatch!
+        //  Ω  = 264°.65,
+            λ  = 199.90538497690034;
+        //  λ  = 199.90895
+
+        console.log(s);
+
+        expect(s.Ω).toBe(Ω);
+        expect(s.λ).toBe(λ);
+
+    });
 
 /*
-
-    var T  = -0.072 183 436,
-
-
-        e  = 0.016 711 668,
-
-
-        R  = 0.997 66,
-        Ω  = 264°.65,
-        λ  = 199°.908 95,
         ε0 = 23°.440 23   (by 22.2),
         ε  = 23°.439 99,
         alpha = 198°.380 83.
         beta  = -7°.785 07
-
 */
-
 
 });
