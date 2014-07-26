f15e5b84253fa5977debd967b5080d7ddc05612e
ufuchs <ufuchs@gmx.com>
2014-07-26 22:58:13 +0200

    added: lib/posas/meeus/rise/

---
+++ spec/meeus/15-rise-spec.js
@@ -0,0 +1,11 @@
+/*jslint node: true */
+/*jslint todo: true */
+/*jslint nomen: true */
+
+'use strict';
+
+var base = require('./../../lib/posas/meeus/base/base.js'),
+    solar = require('./../../lib/posas/meeus/solar/solar.js'),
+    julian = require('./../../lib/posas/meeus/julian/julian.js'),
+    jd_of_1992_Oct_13 = 2448908.5;
+
