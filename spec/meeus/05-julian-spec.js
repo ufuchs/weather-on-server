044a34bfdb41a20159c03e4c776c7b792142e35c
ufuchs <ufuchs@gmx.com>
2014-07-28 20:00:21 +0200

    minor changes on source structur

--- spec/meeus/05-julian-spec.js
+++ spec/meeus/05-julian-spec.js
@@ -16,15 +16,15 @@ var julian = require('./../../lib/posas/meeus/julian/julian.js'),
         [1987,  1, 27.0,  2446822.5],
         [1900,  1,  1.0,  2415020.5],
         [1600,  1,  1.0,  2305447.5],
-        [1957, 10,  4.81, 2436116.31],  // launch time of Sputnik One
+        [1957, 10,  4.81, 2436116.31],          // launch time of Sputnik One
     ],
     date2JdA = [
         // @see: MEEUS, Astronomical Algorithms (Second Edition), p62
         // year, month, day, hour, min, sec, expected JD, expected JD (UTC)
-        [2014, 7, 17,  0, 0, 0, 2456855.5, 2456855.4166666665],     // midnight
-        [2000, 1,  1, 12, 0, 0, 2451545.0, 2451544.9583333335],     // noon
-        [2000, 1,  1,  0, 0, 0, 2451544.5, 2451544.4583333335],     // midnight
-//      [1957, 10, 4, 19, 26, 24, 2436116.31], // launch time of Sputnik One
+        [2014, 7, 17,  0,  0,  0, 2456855.5],   // midnight
+        [2000, 1,  1, 12,  0,  0, 2451545.0],   // noon
+        [2000, 1,  1,  0,  0,  0, 2451544.5],   // midnight
+        [1957, 10, 4, 19, 26, 24, 2436116.31],  // launch time of Sputnik One
 //      [1969, 7, 20, 20, 17, 35, 2440423.3455903], // Apollo 11 moon landing
     ];
 
@@ -39,7 +39,7 @@ describe("A given date by by year, month and day AND hours as decimal places", f
         var actual;
 
         date2Jd.forEach(function (item) {
-            actual = (julian.calendarGregorianToJd.apply(this, item.slice(0, 3)));
+            actual = julian.calendarGregorianToJd.apply(this, item.slice(0, 3));
             expect(item[3]).toBe(actual);
         });
 
@@ -73,7 +73,6 @@ describe("Century since J2K", function () {
     var jd_of_1987_Apr_10 = 2446895.5,
         centuryAtMidnight = -0.12729637234770705;
 
-
     // Meeus, p. 88, example 12.a
     it("returns a value of -0.127296372347... by 1987 April 10 00:00:00.0 UT", function () {
 
@@ -99,7 +98,7 @@ describe("Century since J2K", function () {
 //
 // julian.localTime2utcTime
 //
-describe("Date object to Julian Day number", function () {
+describe("Locale Time to UTC /Greenwich Mean Time", function () {
 
     var f = [
         [2014, 7, 17,  17, 0, 0, 0, 2014,  7, 17,  15, 0, 0, 0],    // midnight
@@ -107,7 +106,7 @@ describe("Date object to Julian Day number", function () {
         [2000, 1,  1,  0, 0, 0, 0, 1999, 12, 31, 23, 0, 0, 0]       // midnight
     ];
 
-    it("returns a Julian Day number", function () {
+    it("returns a UTC date", function () {
 
         var actual,
             expected,
