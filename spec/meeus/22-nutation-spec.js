214d80841b164232abb0b4a1da9ff1e805880c44
ufuchs <ufuchs@gmx.com>
2014-08-03 13:42:52 +0200

    refined: nutation

--- spec/meeus/22-nutation-spec.js
+++ spec/meeus/22-nutation-spec.js
@@ -10,63 +10,50 @@ var base = require('./../../lib/posas/meeus/base.js'),
     julian = require('./../../lib/posas/meeus/julian.js'),
     jd_of_1988_April_10 = 2446895.5;
 
-
-function x(a) {
-
-    var deg = ~~a,
-        f = a - deg,
-        sec,
-
-        min = f * 60;
-        sec = (min - ~~min) * 60;
-        min = ~~min;
-
-    console.log(deg, min, sec);
-}
-
 //
 //
 //
-describe("", function () {
+describe("Obliqity of Ecliptic", function () {
 
-    var T = //-0.127296372348,
-            julian.j2000Century(jd_of_1988_April_10),
+    var T = julian.j2000Century(jd_of_1988_April_10),
         actual,
-        expected = 108.53437047264285;
+        expected = 23.440946290957324,
 
+        expected_Δε = 0.002629995763497817,     //  9°.46798474859214
+        expected_Δψ = -0.0010729882875282794;   // -3°.8627578351018057
 
-    console.log(T);
-
-    it("", function () {
-
-        actual = nutation.calcMeanObliquityLaskar(T);//calcMeanObliquity(T);
-
-        console.log(actual);
-
-        x(actual);
-
-        var n = nutation.calcApproxNutation(T);
-
-        console.log(n);
-
-        actual += n.Δε;
-
-        x(actual);
-
-        x(n.Δψ);
-
-
-    });
-
-    it("", function () {
+    it("gets the 'Mean Obliquity'", function () {
 
         actual = nutation.calcMeanObliquityLaskar(T);
 
-//        console.log(actual);
-
-//        expect(expected).toBe(actual.H0);
+        expect(actual).toBe(expected);
 
     });
 
+    it("gets the correction of the 'Mean Obliquity'", function () {
+
+        actual = nutation.calcApproxNutation(T);
+
+//      console.log(base.degdec2degmmss(actual.Δψ));
+//      console.log(base.degdec2degmmss(actual.Δε));
+
+        expect(actual.Δε).toBe(expected_Δε);
+        expect(actual.Δψ).toBe(expected_Δψ);
+
+    });
+
+    it("gets the 'True Obliquity'", function () {
+
+        var n = nutation.calcApproxNutation(T),
+            expected_ε = 23.443576286720823;
+
+        actual = nutation.calcMeanObliquityLaskar(T) + n.Δε;
+
+//      console.log(base.degdec2degmmss(actual));
+
+        expect(actual).toBe(expected_ε);
+
+
+    });
 
 });
