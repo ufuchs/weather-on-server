0ca7e7a3391c0bbe63e44827749131f51dd02875
ufuchs <ufuchs@gmx.com>
2014-08-19 22:55:43 +0200

    added: interpol test

--- spec/meeus/01-base-spec.js
+++ spec/meeus/01-base-spec.js
@@ -3,7 +3,7 @@
 /*jslint nomen: true */
 
 
-var base = require('./../../lib/posas/meeus/base.js');
+var base = require('./../../lib/sunJS/meeus/base.js');
 
 describe("Positive Modulo", function () {
 
@@ -35,4 +35,19 @@ describe("Horner algorithm", function () {
     });
 
 });
+
+describe("interpolation", function () {
+
+    it("returns a value of 5", function () {
+
+        var values = [0.884226, 0.877366, 0.870531],
+            actual = base.interpol(values, 0.18125),
+            expected = 0.8761253012695313;
+
+        expect(expected).toBe(actual);
+
+    });
+
+});
+
 
