773339f1b6ad744ce3f00e088853dd1abe0c184b
ufuchs <ufuchs@gmx.com>
2014-08-22 22:55:05 +0200

    tidy up

--- spec/meeus/12-sidereal-spec.js
+++ spec/meeus/12-sidereal-spec.js
@@ -8,56 +8,51 @@
 var julian = require('./../../lib/sunJS/meeus/julian.js'),
     base = require('./../../lib/sunJS/meeus/base.js'),
     deltat = require('./../../lib/sunJS/meeus/deltat.js'),
-    sidereal = require('./../../lib/sunJS/meeus/sidereal.js'),
-    jd_of_1987_Apr_10 = 2446895.5;
+    sidereal = require('./../../lib/sunJS/meeus/sidereal.js');
 
 //
 //
 //
-describe("Mean Sidereal Time at Greenwich", function () {
+describe("Mean Sidereal Time at Greenwich on 1987 Apr 10", function () {
+
+    var jd_of_1987_Apr_10 = 2446895.5,
+        expected_gmst0 = 0.5491477642026628,
+        expected_gmst_1921 = 0.35760520352271286,
+        expected_gmst_0721 = 0.856236248847713;
 
     // calcGmst0
     // Meeus, p. 88, part of example 12.a
-    it("returns in pure seconds at 00:00", function () {
+    it("gets the fraction of a day at UT 0h", function () {
 
-        var actual,
-            expected_gmst0 = 0.5491477642026628;
+        var gmst0 = sidereal.calcGmst0(jd_of_1987_Apr_10);
 
-        actual = sidereal.calcGmst0(jd_of_1987_Apr_10),
-
-        // 13h 10m 46".3668 2711006142
-        console.log('gmst0', julian.dec2hhmmss(actual));
-
-        expect(actual).toBe(expected_gmst0);
-
+        // 13h 10m 46".366 827 110 061 42
+        // console.log('gmst0', julian.dec2hhmmss(gmst0));
+        expect(gmst0).toBe(expected_gmst0);
     });
 
     // Meeus, p. 89, part of example 12.b
-    it("returns in pure seconds at 19:21", function () {
+    it("gets the fraction of a day at UT 19h 21''", function () {
 
-        var actual = sidereal.calcGmst(jd_of_1987_Apr_10
-                + julian.hhmmss2dec(19, 21, 0)),
-            expected_gmst = 0.35760520352271286;
+        var gmst = sidereal.calcGmst(jd_of_1987_Apr_10
+            + julian.hhmmss2dec(19, 21, 0));
 
         // 8h 34m 57".0895 8436240209
-//        console.log('gmst', julian.dec2hhmmss(actual) );
-
-        expect(actual).toBe(expected_gmst);
-
+        // console.log('gmst', julian.dec2hhmmss(gmst) );
+        expect(gmst).toBe(expected_gmst_1921);
     });
 
     // Meeus, p. 89, part of example 12.b
-    it("returns in pure seconds at 07:21", function () {
+    it("returns in pure seconds at UT 07:21", function () {
 
-        var actual = sidereal.calcGmst(jd_of_1987_Apr_10
-                + julian.hhmmss2dec(7, 21, 0)),
-            expected_gmst = 0.856236248847713;
+        var gmst = sidereal.calcGmst(jd_of_1987_Apr_10
+            + julian.hhmmss2dec(7, 21, 0));
+
 
         // 20h 32' 58".8119 0044240793
 //        console.log('gmst', julian.dec2hhmmss(actual));
 
-        expect(actual).toBe(expected_gmst);
-
+        expect(gmst).toBe(expected_gmst_0721);
     });
 
 
@@ -66,51 +61,32 @@ describe("Mean Sidereal Time at Greenwich", function () {
 //
 //
 //
-describe("Apparent Sidereal Time at Greenwich", function () {
+describe("Apparent Sidereal Time at Greenwich on 1987 Apr 10", function () {
 
-    // Meeus, p. 88, part of example 12.a
-    it("returns in pure seconds at 00:00", function () {
-
-        // (m)ean0UT
-        var actual = sidereal.calcGast0(jd_of_1987_Apr_10),
-            expected_gast0 = 0.5491450826854554;
-
-        // 13h 10m 46".1351
-//        console.log('gast0', julian.dec2hhmmss(actual));
-
-        expect(actual).toBe(expected_gast0);
-
-    });
-
-
-    // Meeus, p. 88, part of example 12.a
-    // it("returns in pure seconds at 00:00", function () {
-
-    //     // (m)ean0UT
-    //     var actual = sidereal.calcGmstEx(jd_of_1987_Apr_10),
-    //         expected_gast0 = 0.5491450826854554;
-
-    //     // 13h 10m 46".1351
-    //     console.log('gast0', julian.dec2hhmmss(actual));
-
-    //     expect(actual).toBe(expected_gast0);
-
-    // });
-
-
-
-    // Meeus, p. 88, part of example 12.a
-    it("returns in pure seconds at 19:21", function () {
-
-        // (m)ean0UT
-        var actual = sidereal.calcGast(jd_of_1987_Apr_10 +
-                julian.hhmmss2dec(19, 21, 0)),
+        var jd_of_1987_Apr_10 = 2446895.5,
+            expected_gast0 = 0.5491450826854554,
             expected_gast = 0.3576024658368484;
 
-        // 8° 34' 56".8579 012756818
-//        console.log('gast', julian.dec2hhmmss(actual));
+    // Meeus, p. 88, part of example 12.a
+    it("returns in pure seconds at UT 00:00", function () {
 
-        expect(actual).toBe(expected_gast);
+        var gast0 = sidereal.calcGast0(jd_of_1987_Apr_10);
+
+        // 13h 10m 46".1351
+        // console.log('gast0', julian.dec2hhmmss(gast0));
+        expect(gast0).toBe(expected_gast0);
+    });
+
+    // Meeus, p. 88, part of example 12.a
+    it("returns in pure seconds at UT 19:21", function () {
+
+        var gast = sidereal.calcGast(jd_of_1987_Apr_10 +
+                julian.hhmmss2dec(19, 21, 0));
+
+        // 8° 34' 56".8579 012756818
+        // console.log('gast', julian.dec2hhmmss(actual));
+
+        expect(gast).toBe(expected_gast);
 
     });
 
